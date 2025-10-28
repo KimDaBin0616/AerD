9ecad032943c385906be9dd2d96e2a304775ca37
Park woojin <123284655+sosehfdl@users.noreply.github.com>
2025-10-28 16:01:02 +0900

    Delete 2stage directory

--- 2stage/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-GroundingVLM package for military object detection and classification
-"""
-
-from GroundingVLM.grounding_utils import *
-from GroundingVLM.coco_utils import *
-from GroundingVLM.process import * 
